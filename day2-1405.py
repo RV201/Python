74e0029711dca5a0e8f0bbe562778a8bda47b46a
Rakshitha R <108003363+RV201@users.noreply.github.com>
2024-05-14 22:02:32 +0530

    Add files via upload

---
+++ day2-1405.py
@@ -0,0 +1,17 @@
+#To print two numbers
+def switch(i):
+    a = int(input("Enter first value"))
+    b = int(input("Enter second value"))
+    i= int(input("Press 1 for addition 2 for subtraction"))
+    if i=="1":
+        return a+b
+    elif i=="2":
+        return a-b
+    else:
+        print("invalid")
+
+
+
+
+
+        
