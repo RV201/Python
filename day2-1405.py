2d894bd5dd9a75aa720cdde65615eb772bbf28e3
Rakshitha R <108003363+RV201@users.noreply.github.com>
2024-05-15 22:20:05 +0530

    Add files via upload

--- day2-1405.py
+++ day2-1405.py
@@ -1,17 +1,6 @@
-#To print two numbers
-def switch(i):
-    a = int(input("Enter first value"))
-    b = int(input("Enter second value"))
-    i= int(input("Press 1 for addition 2 for subtraction"))
-    if i=="1":
-        return a+b
-    elif i=="2":
-        return a-b
-    else:
-        print("invalid")
-
-
-
-
-
-        
+print('\n Normal pyramid')
+for i in range(5):
+    x='* '
+    x=x*i
+    print(f' {x:^10}')
+    
---
+++ day3-1505.py
@@ -0,0 +1,10 @@
+#+,-,* of 2 nos.
+x=int(input('Enter first number \n'))
+y=int(input('Enter second number'))
+print('The sum of {} and {} is {}' .format(x,y,x+y))
+print('The difference of {} and {} is {}' .format(x,y,x-y))
+print('The product of {} and {} is {} '.format(x,y,x*y))
+
+
+
+
