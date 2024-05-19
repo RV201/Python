7b10709b495c5d9a494a81211a2bd4bcc80ded36
Rakshitha R <108003363+RV201@users.noreply.github.com>
2024-05-19 19:33:57 +0530

    Add files via upload

---
+++ day6-1905.py
@@ -0,0 +1,13 @@
+#To check prime no.
+num = int(input("enter a number: "))
+flag = False
+if num > 1:
+    for i in range(2, num):
+        if (num % i) == 0:
+            flag = True
+            break
+
+if flag:
+    print(num, "is not a prime number")
+else:
+    print(num, "is a prime number")
