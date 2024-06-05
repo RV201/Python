df44233c2c3fcb7316d40072b5f1e9469ec86857
Rakshitha R <108003363+RV201@users.noreply.github.com>
2024-06-05 08:51:32 +0530

    Add files via upload

---
+++ day25-0506.py
@@ -0,0 +1,15 @@
+a = int(input('Enter first number  : '))
+# 12
+b = int(input('Enter second number : '))
+# 14
+c = int(input('Enter third number  : '))
+# 11
+smallest = 0
+if a < b and a < c :
+    smallest = a
+if b < a and b < c :
+    smallest = b
+if c < a and c < b :
+    smallest = c
+print(smallest, "is the smallest of three numbers.")
+# 11 is the smallest of three numbers.
