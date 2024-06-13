e7d961c8389261399eef3ddcec9adaeb8f6844cf
Rakshitha R <108003363+RV201@users.noreply.github.com>
2024-06-13 22:40:33 +0530

    Add files via upload

---
+++ day33-1306.py
@@ -0,0 +1,6 @@
+#take input from the user
+num = int(input("Enter a number to calculate square : "))
+print("square =",num*num)
+#take input from the user
+num = int(input("Enter a number to calculate cube : "))
+print("square =",num*num*num)
