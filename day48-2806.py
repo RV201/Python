ce1ba9f2062e242e6cf975a653288039bd304610
Rakshitha R <108003363+RV201@users.noreply.github.com>
2024-06-28 23:50:06 +0530

    Add files via upload

---
+++ day48-2806.py
@@ -0,0 +1,5 @@
+#taking inputs from the user
+str1 = input('Enter first string: ')
+str2 = input('Enter second string: ')
+#printing the output after using join() method
+print("String after concatenation :","".join([str1, str2]))
