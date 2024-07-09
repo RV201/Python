4c9a8a0d6a33a62a99cc1c42a6dd9275ab269b81
Rakshitha R <108003363+RV201@users.noreply.github.com>
2024-07-09 23:27:47 +0530

    Add files via upload

---
+++ day59-0907.py
@@ -0,0 +1,24 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = Noneclass LinkedList:
+    def __init__(self):
+        self.head = Nonedef insert_at_end(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            return
+        last_node = self.head
+        while last_node.next:
+            last_node = last_node.next
+        last_node.next = new_node
+    def traverse(self):
+        current_node = self.head
+        while current_node:
+            print(current_node.data, end=" -> ")
+            current_node = current_node.nextprint("None")
+linked_list = LinkedList()
+linked_list.insert_at_end(1)
+linked_list.insert_at_end(2)
+linked_list.insert_at_end(3)
+linked_list.traverse()
