96ffa774f50a8bf22af21a07c9fc49a938247771
Rakshitha R <108003363+RV201@users.noreply.github.com>
2024-07-18 23:18:19 +0530

    Add files via upload

---
+++ day68-1807.py
@@ -0,0 +1,13 @@
+# Importing Regular Expressions Library
+import re
+
+name = 'Python is a wonderful language, it is best 1 in current tech'
+
+digitCount = re.sub("[^0-9]", "", name)
+letterCount = re.sub("[^a-zA-Z]", "", name)
+spaceCount = re.findall("[ \n]", name)
+
+print(len(digitCount))
+print(len(letterCount))
+print(len(spaceCount))
+    
