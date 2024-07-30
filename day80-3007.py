9be160ff63764e49af25ff563e3513a88e4664c5
Rakshitha R <108003363+RV201@users.noreply.github.com>
2024-07-30 23:05:04 +0530

    Add files via upload

---
+++ day80-3007.py
@@ -0,0 +1,10 @@
+class Dog:
+
+    # Class attributes
+    kingdom = "Animalia"
+    species = "Canis lupus"
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        
