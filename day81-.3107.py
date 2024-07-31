7c6bd39abb30c3b9d90d03bc07096290a439ddab
Rakshitha R <108003363+RV201@users.noreply.github.com>
2024-07-31 22:41:12 +0530

    Add files via upload

---
+++ day81-.3107.py
@@ -0,0 +1,7 @@
+# input the base and exponent
+base = int(input("Enter the base number: "))
+exponent = int(input("Enter the exponent: "))
+# calculate the power using the pow method
+result = pow(base, exponent)
+# print the result
+print("The result of {} raised to the power of {} is {}".format(base, exponent, result))
