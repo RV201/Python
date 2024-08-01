6c8c9935aad4599a229133b8a1aca66467801eb4
Rakshitha R <108003363+RV201@users.noreply.github.com>
2024-08-01 22:08:42 +0530

    Add files via upload

---
+++ day82-0108.py
@@ -0,0 +1,12 @@
+
+numb = int(input(" Enter a number : "))    
+
+# using the for loop to generate the multiplication tables     
+
+print("Table of: ")  
+
+for a in range(1,11):    
+
+   print(num,'x',a,'=',num*a)  
+
+
