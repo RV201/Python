bcfabecb878d5c32cecb9849fa8b3d85d69ccca9
Rakshitha R <108003363+RV201@users.noreply.github.com>
2024-09-24 22:58:48 +0530

    Add files via upload

---
+++ Check greater or lesser.py
@@ -0,0 +1,8 @@
+a = int(input("Enter the first number"))
+b = int(input('Enter the second number'))
+if b > a:
+  print("b is greater than a")
+elif(a>b):
+  print("a is greater than b")
+else:
+  print("a is equal to b")
